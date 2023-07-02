3de2eeed658e203df8da13e1e78e7217b02e78c4
«Viktoria» <vika.mel.2015@mail.ru>
2023-07-02 18:21:44 +0300

    sem3online

---
+++ Seminar3/Seminar3_Online.java
@@ -0,0 +1,15 @@
+package Seminar3;
+
+import java.util.Random;
+
+public class Seminar3_Online {
+    
+    public static void main(String[] args) {
+        Random random = new Random();
+        while(true){
+            System.out.println(random.nextInt(10    ));
+        }
+        
+    }
+    
+}
